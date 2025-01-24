5c62a5fed225f6b68c328f32ba5b2731f6cdba3f
mbuckne2 <mbuckne2@asu.edu>
2025-01-23 19:33:28 -0700

    added generic memory allocation test function

--- helpers.py
+++ helpers.py
@@ -1,6 +1,5 @@
-import math
 import os
-import random
+import pandas as pd
 
 import timeout_decorator
 
@@ -39,3 +38,36 @@ def remove_file(file_name):
         os.remove(file_name)
     except:
         pass
+
+
+#TODO: there is now size, line, and file of each malloc and free
+# will be useful for generating student feedback
+
+# returns malloc data
+#
+# num mallocs
+# num frees
+# [mallocs without frees]
+# [frees without mallocs]
+# dataframe
+#
+def test_mallocs():
+    column_names = ['Type', 'Address', 'Size', 'Line', 'File']
+    try:
+        df = pd.read_csv("malloc_log.csv", header=None, names=column_names)
+    except:
+        return [0, 0, [], []]
+
+    mallocs = df[df['Type'].isin(['MALLOC', 'CALLOC'])]
+    frees = df[df['Type'] == 'FREE']
+
+    # check for address parity
+    # Filter the DataFrame to get MALLOC and FREE addresses separately
+    malloc_addresses = df[df['Type'] == 'MALLOC']['Address']
+    free_addresses = df[df['Type'] == 'FREE']['Address']
+
+    # Check if MALLOC addresses all match FREE addresses
+    mallocs_without_frees = malloc_addresses[~malloc_addresses['Address'].isin(free_addresses['Address'])]
+    frees_without_mallocs = free_addresses[~free_addresses['Address'].isin(malloc_addresses['Address'])]
+
+    return mallocs.shape[0], frees.shape[0], mallocs_without_frees, frees_without_mallocs, df
